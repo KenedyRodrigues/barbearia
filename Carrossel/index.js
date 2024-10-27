3a98fe780291a8569139f81aca3e7e687cf787b2
KenedyRodrigues <165099685+KenedyRodrigues@users.noreply.github.com>
2024-10-27 16:41:20 -0300

    Add files via upload

--- Carrossel/index.js
+++ Carrossel/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Image, Dimensions } from 'react-native';
-import Swiper from 'react-native-swiper';
-
-/* npm install --save react-native-swiper */
-
-const { width: viewportWidth } = Dimensions.get('window');
+import { FontAwesome, MaterialCommunityIcons as Icon } from '@expo/vector-icons';
+import Swiper from 'react-native-swiper'; // npm install --save react-native-swiper
+import styles from './style';
+import { Text,View, ScrollView, Image} from 'react-native'; 
 
 const images = [
   require('./imagens/barbearia.png'),
@@ -12,37 +10,51 @@ const images = [
   require('./imagens/barbearia.png'),
 ];
 
-const ImageCarousel = () => {
+export default function Carrossel() {
   return (
+   <ScrollView style={styles.container} scrollEnabled={false}>
+     <View style={styles.iconeView}>    
+      <FontAwesome name="user" size={40} color="#000" /> 
+      <Text style={styles.textoPerfil}>Meu perfil</Text>  
+      <FontAwesome name="bars" size={30}  color="white" />
+    </View>
+    <View style={styles.textView}>
+    <Text style={styles.textPrincipal}>Escolha o profissional de sua preferência e reserve um horário</Text>
+    </View>
+    <Text style={styles.textServico}>Serviços</Text>
+    <View style={styles.viewServico}>
+      <View>
+        <Icon style={styles.viewIcones} name="content-cut" size={40} color="#000" />
+      </View>
+      <View>
+      <Icon style={styles.viewIcones} name="mustache" size={40} color="#000" />
+      </View>
+      <View>
+      <Icon style={styles.viewIcones} name="razor-double-edge" size={40} color="#000" />
+      </View>
+    </View>
+    <View style={styles.containerTextServico}>
+      <Text style={styles.servicoText}>Corte completo</Text>
+      <Text style={styles.servicoText}>Barba e bigode</Text>
+      <Text style={styles.servicoText}>Pezinho e sobrancelha</Text>
+    </View>
+    <View > 
+      <Text style={styles.textServico} >Galeria</Text>
+    </View>
+
     <Swiper
       style={styles.wrapper}
-      showsButtons={true}
+      showsButtons={false}
       autoplay={true}
-      autoplayTimeout={3} // tempo em segundos para cada slide
+      autoplayTimeout={3} // Tempo em segundos para cada slide
     >
       {images.map((image, index) => (
         <View key={index} style={styles.slide}>
-          <Image source={image} style={styles.image} />
+          <Image source={image} style={styles.image1} />
         </View>
       ))}
     </Swiper>
+
+   </ScrollView>
   );
-};
-
-const styles = StyleSheet.create({
-  wrapper: {
-    height: 10000, // Altura do carrossel
-  },
-  slide: {
-    width: viewportWidth,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    width: '50%',
-    height: '50%',
-    resizeMode: 'cover',
-  },
-});
-
-export default ImageCarousel;
+}
--- Carrossel/style.js
+++ Carrossel/style.js
@@ -1,42 +1,113 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
-export default StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000a1f",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  imageContainer: {
-    width: 140,
-    height: 140,
-    position: "relative",
-  },
-  imageWrapper: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  image: {
-    width: 140,
-    height: 140,
-    position: "absolute",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    marginTop: 40,
-  },
-  button: {
-    backgroundColor: "#162645",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginHorizontal: 20,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-  },
-});
+const { width: viewportWidth } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    
+    container:{
+        width: '100%'
+    },
+    iconeView:{
+        padding: 70,
+        flexDirection:'row',
+        justifyContent: 'space-between'
+    
+    },
+    
+    textoPerfil:{
+        color:"#fff",
+        fontSize: 21,
+        fontWeight: 'semibold',
+        textAlign: 'left',
+        paddingRight: 170
+    },
+    textView:{
+        width: '80%',
+        margin: 'auto',
+        marginTop: -20
+    },
+    textPrincipal:{
+        color: 'white',
+        fontFamily:'Roboto' ,
+        fontSize:19,
+        fontWeight:'semibold',
+        marginTop: 0,
+        textAlign: 'center',
+    },
+    textServico:{
+        
+        color: 'white',
+        fontFamily:'Playfair Display' ,
+        fontSize:19,
+        fontWeight:'semibold',
+        marginTop: 0,
+        textAlign: 'left',
+        paddingLeft: 50,
+        paddingTop: 30
+
+    },
+    viewServico:{
+        width:'100%',
+        flexDirection:'row',
+        justifyContent: 'space-around',
+        margin: 10
+    },
+    viewIcones:{
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding:10,
+        
+    },
+    containerTextServico:{
+        
+        width:'100%',
+        flexDirection:'row',
+        justifyContent: 'space-around',
+        margin: 10
+    },
+    servicoText:{
+        
+        color: '#A9A9A9',
+        fontFamily:'Roboto' ,
+        fontSize:14,
+        fontWeight:'normal',
+    },
+
+
+    galeria:{
+        color: 'white',
+        fontFamily:'Playfair Display' ,
+        fontSize:19,
+        fontWeight:'semibold',
+        marginTop: 0,
+        textAlign: 'left',
+        paddingLeft: 50,
+        paddingTop: 30
+    },
+    image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+    },
+
+    wrapper: {
+      marginTop: 23,
+      height: 250, // Ajuste a altura do carrossel conforme necessário
+    },
+    slide: {
+      width: viewportWidth, // Usa a largura da tela
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    image1: {
+      width: 150, // Defina a largura desejada
+      height: 150, // Defina a altura desejada
+      borderRadius: 75, // Torna a imagem redonda
+      resizeMode: 'cover',
+    },
+    
+    
+
+})
+
+export default styles;
